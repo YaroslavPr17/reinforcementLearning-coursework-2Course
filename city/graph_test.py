2017ac31594335c7d9815143950df1f9b406b8ba
YaroslavPr17 <yarikpristalov1702@yandex.ru>
2022-06-08 22:01:18 +0300

    Add outer graph_test

---
+++ city/graph_test.py
@@ -0,0 +1,8 @@
+from graphics.env_vizualization import MapVizualization
+from Environment.city import City
+
+from Agent.agent import Agent
+
+a = Agent(5)
+a.extract_q_table(Agent.load_compressed_q_table_from_file('compressed_q_table'))
+a.perform(11)
